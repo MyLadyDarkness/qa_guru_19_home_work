25f7f6d855c23f930b966a3cbe75235418d087da
MyLadyDarkness <Cleopathra@yandex.ru>
2024-06-03 12:25:25 +0300

    attempt move attach

--- selene_in_action/utils/allure.py
+++ selene_in_action/utils/allure.py
@@ -1,10 +1,11 @@
 import allure
+from selene import browser
+import requests
+from allure_commons.types import AttachmentType
 import config
 
 
 def attach_bstack_video(session_id):
-
-    import requests
     bstack_session = requests.get(
         f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
         auth=(config.bstack_userName, config.bstack_accessKey),
@@ -20,4 +21,42 @@ def attach_bstack_video(session_id):
         '</body></html>',
         name='video recording',
         attachment_type=allure.attachment_type.HTML,
+    )
+
+
+def attach_bstack_screen():
+    allure.attach(
+        browser.driver.get_screenshot_as_png(), #get_screenshot_as_png(),
+        name='screenshot',
+        attachment_type=AttachmentType.PNG,
+        extension='.png'
+    )
+
+def attach_screen():
+    allure.attach(
+        browser.driver.get_screenshot_as_png(),
+        name='screenshot',
+        attachment_type=allure.attachment_type.PNG
+    )
+
+def add_screenshot(browser):
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png,
+                  name='Screenshot',
+                  attachment_type=allure.attachment_type.PNG)
+
+
+# def add_screenshot(browser):
+#     png = browser.driver.get_screenshot_as_png()
+#     allure.attach(body=png,
+#                   name='screenshot',
+#                   attachment_type=allure.attachment_type.PNG,
+#                   extension='.png')
+
+
+def attach_bstack_source(browser):
+    allure.attach(
+        browser.driver.page_source,
+        name='screen xml dump',
+        attachment_type=allure.attachment_type.XML,
     )
